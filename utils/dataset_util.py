71cf9acbd61e981b9f995741022060812e812a1d
Wilson Ng <wilsonng234@gmail.com>
2022-11-17 22:02:33 +0800

    Refactor split_data_folders

--- utils/dataset_util.py
+++ utils/dataset_util.py
@@ -1,11 +1,9 @@
 import torch
 from torchvision import datasets, transforms
+
 import pandas as pd
-import numpy as np
 import os
-import glob
 import shutil
-from sys import exit
 
 def get_dataloader(batch_size, img_size):
     transform = transforms.Compose([
@@ -19,49 +17,40 @@ def get_dataloader(batch_size, img_size):
 
     return dataloader
 
+def split_data_folders():  
+    def folders_create(id, label):
+        for i in set(label):
+            mkdir(str(i))
 
-def transform_data():
+    def read_csv():
+        img = pd.read_csv('datasets/images.csv')
+
+        return img
+
+    def mkdir(path):
+        path1 = os.path.join('datasets/classes', path)
+        folder1 = os.path.exists(path1)
+        if not folder1:                   
+            os.makedirs(path1)
+            
+    def copy_files(id, label):
+        for i in range(len(id)):
+            name = str(id[i])
+            folder = str(label[i])
+            file_name = name+'.jpg'
+
+            img_source = os.path.join('datasets/images', file_name)
+            img_target = os.path.join('datasets/classes', folder, file_name)
+            
+            try:
+                if not os.path.exists(img_target):
+                    shutil.copy(img_source,img_target)
+            except FileNotFoundError:   # some image files are corrupted and be deleted
+                pass
+
     img = read_csv()
     id = img['image']
     label = img['label']
-    if not os.path.exists('compressed'):
-        os.makedirs('compressed')
-    folder_create(id,label)
-    copy_file(id,label)
-
-
-def folder_create(id,label):
     
-    for i in label:
-        mkdir(str(i))
-    
-def read_csv():
-    img = pd.read_csv('images.csv')
-    print(img.head(10))
-    return img
-
-
-def mkdir(path):
-    path1 = os.path.join('compressed',path)
-    path2 = os.path.join('original',path)
-    folder1 = os.path.exists(path1)
-    folder2 = os.path.exists(path2)
-    if not folder1:                   
-	    os.makedirs(path1)
-    if not folder2:
-        os.makedirs(path2)
-
-
-def copy_file(id,label):
-    for i in range(len(id)):
-        name = str(id[i])
-        folder = str(label[i])
-        file_name = name+'.jpg'
-        # print(source_file)
-        compressed_source = os.path.join('./images_compressed',file_name)
-        compressed_target = os.path.join('./compressed',folder,file_name)
-        # print(compressed_target)
-        # print(compressed_source)
-        
-        if not os.path.exists(compressed_target):
-            shutil.copy(compressed_source,compressed_target)
+    folders_create(id, label)
+    copy_files(id, label)
