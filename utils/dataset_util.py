ffe144a488fb92a767eac87010983cfad68b0a8c
Wilson Ng <wilsonng234@gmail.com>
2022-11-18 23:29:58 +0800

    Create combine_edges function

--- utils/dataset_util.py
+++ utils/dataset_util.py
@@ -7,6 +7,8 @@ import pandas as pd
 import os
 from os import listdir
 import shutil
+import cv2
+import numpy as np
 
 def get_dataloader(batch_size, img_size):
     transform = transforms.Compose([
@@ -36,6 +38,46 @@ def remove_background():
         output = rembg.remove(input)
         output = output.convert('RGB')
         output.save(output_path)
+
+def combine_edges(dataloader, img_channel=3, img_size=240):
+    def merge(edges, original_img):
+        blank_space = 12
+        combined_img = np.zeros((edges.shape[0], edges.shape[1]*2 + blank_space, img_channel))
+        combined_img[:, edges.shape[1]:edges.shape[1]+blank_space, :] = 255
+
+        combined_img[:, 0:edges.shape[1], :] = edges
+        combined_img[:, edges.shape[1]+blank_space : (edges.shape[1]*2)+blank_space, :] = original_img
+
+        return combined_img
+
+    output_path = 'datasets/combined_images'
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    cnt = 1
+    for _, (images, _) in enumerate(dataloader):
+        for _, img in enumerate(images):
+            img = img.numpy()
+            img = img.transpose(1, 2, 0)    # 3, 255, 255 -> 255, 255, 3
+
+            img_diff = np.ones(img.shape) * 255 - img
+            img_diff = np.uint8(img_diff)
+
+            threshold = 200
+            img_diff[img_diff < threshold] = 0
+            img_diff[img_diff >= threshold] = 255
+            
+            edges = cv2.Canny(image=img_diff, threshold1=90, threshold2=200)  # Canny Edge Detection
+            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((3, 3),np.uint8))
+
+            edges = np.ones(edges.shape) * 255 - edges
+            edges = np.uint8(edges)
+            edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+            
+            combined = merge(edges, (img+1)/2*255)
+            cv2.imwrite(os.path.join(output_path, str(cnt) + "_combined.jpg"), combined)
+            cnt += 1
 
 def split_data_folders():  
     def read_csv():
