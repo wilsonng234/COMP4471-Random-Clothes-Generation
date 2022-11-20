15d3ae85ddc0420a3ffc71e172473f8e98912da7
Wilson Ng <wilsonng234@gmail.com>
2022-11-20 22:34:41 +0800

    Update config

--- model/pix2pix/config.py
+++ model/pix2pix/config.py
@@ -4,3 +4,8 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 32
 IMG_CHANNELS = 3
 IMG_SIZE = 256
+BLANK_SPACE = 12
+
+TRAIN_DIR = None
+VAL_DIR = None
+TEST_DIR = None
