930614e2ef9c7c74564f4bbdf17d5d721d018611
Wilson Ng <csngae@connect.ust.hk>
2022-12-01 21:20:26 +0800

    Update data augmentation

--- model/pix2pix/config.py
+++ model/pix2pix/config.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+from torchvision import transforms
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 32
@@ -11,3 +13,13 @@ VAL_DIR = None
 TEST_DIR = None
 
 LOAD_MODEL = False
+TRAIN_MODEL = False
+
+augmentation_transform = transforms.RandomApply(
+    nn.ModuleList(
+        [
+            transforms.ColorJitter(brightness=0, contrast=0, saturation=0.2, hue=0.2),
+            transforms.RandomRotation(degrees=(45, 45))
+        ]
+    ), p=1/3
+)
