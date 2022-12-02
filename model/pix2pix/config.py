b74aa503fd3837f45e577ab76b0d782b757003ae
Wilson Ng <csngae@connect.ust.hk>
2022-12-02 16:46:05 +0800

    Update color jitter setting

--- model/pix2pix/config.py
+++ model/pix2pix/config.py
@@ -20,7 +20,7 @@ MODEL_PATH = "checkpoints"
 augmentation_transform = transforms.RandomApply(
     nn.ModuleList(
         [   
-            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.5),
             transforms.RandomHorizontalFlip(p=0.5),
             # transforms.RandomRotation(degrees=(10, 10))
         ]
