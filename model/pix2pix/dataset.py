282a7b61fa17644474107bcced1f2d5d3ffe272b
Wilson Ng <csngae@connect.ust.hk>
2022-12-04 00:51:07 +0800

    Updata data augmentation on edges

--- model/pix2pix/dataset.py
+++ model/pix2pix/dataset.py
@@ -3,16 +3,18 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 import os
+from . import config
+import random
 import numpy as np
 from PIL import Image
 
 class ClothingDataset(Dataset): 
-    def __init__(self, img_size, blank_space, images_dir, augmentation_transform=None):
+    def __init__(self, img_size, blank_space, images_dir, augmentation=True):
         self.img_size = img_size
         self.blank_space = blank_space
         self.images_dir = images_dir
         self.images_name = os.listdir(images_dir)
-        self.augmentation_transform = augmentation_transform
+        self.augmentation = augmentation
 
     def __len__(self):
         return len(self.images_name)
@@ -26,9 +28,20 @@ class ClothingDataset(Dataset):
         edge_img = img[:, :self.img_size, :]
         original_img = img[:, (self.img_size+self.blank_space):, :]
 
-        if self.augmentation_transform is not None:
+        if self.augmentation:
             original_img = transforms.ToPILImage()(original_img)
-            original_img = self.augmentation_transform(original_img)
+            edge_img = transforms.ToPILImage()(edge_img)
+            
+            colorJitter = transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.5)
+            horizontalFlip = transforms.RandomHorizontalFlip(p=1)
+            
+            threshold = 1/3
+            if random.random() <= threshold:
+                original_img = colorJitter(original_img)
+
+            if random.random() <= threshold:
+                original_img = horizontalFlip(original_img)
+                edge_img = horizontalFlip(edge_img)
 
         both_transform = transforms.Compose([
             transforms.ToTensor(),
@@ -38,19 +51,14 @@ class ClothingDataset(Dataset):
         edge_img = both_transform(edge_img)
         original_img = both_transform(original_img)
 
-        # edge_img = edge_img.transpose(2, 0, 1)
-        # original_img = original_img.transpose(2, 0, 1)
-        
-        # edge_img = (edge_img/125.0) -1
-        # original_img = (original_img/125.0) -1
-
         return edge_img, original_img
 
     def get_dataloader(self, batch_size, shuffle=True):
         loader = DataLoader(
             self,
             batch_size=batch_size,
-            shuffle=True
+            shuffle=True,
+            num_workers=config.NUM_WORKERS
         )
 
         return loader
