1e09256dc4fbc4ea9a0fc16d6073306fb28e34bf
Wilson Ng <csngae@connect.ust.hk>
2022-12-05 04:25:15 +0800

    Remove augmentation_trasform in config

--- model/pix2pix/config.py
+++ model/pix2pix/config.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torchvision import transforms
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 IMG_CHANNELS = 3
 IMG_SIZE = 256
 BLANK_SPACE = 12
@@ -19,13 +19,3 @@ TENSORBOARD_DIR = "tensorboard"
 
 LOAD_MODEL = False
 MODEL_PATH = "checkpoints"
-
-augmentation_transform = transforms.RandomApply(
-    nn.ModuleList(
-        [   
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.5),
-            transforms.RandomHorizontalFlip(p=0.5),
-            # transforms.RandomRotation(degrees=(10, 10))
-        ]
-    ), p=1/3
-)
