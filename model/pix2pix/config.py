72ca9d8b832ee4dfe1c30ab0732e65d6cd247d3b
Wilson Ng <wilsonng234@gmail.com>
2023-02-06 00:33:36 +0800

    Update config.py

--- model/pix2pix/config.py
+++ model/pix2pix/config.py
@@ -1,6 +1,21 @@
 import torch
-import torch.nn as nn
-from torchvision import transforms
+from os import walk
+import re
+
+def get_current_epoch():
+    current_epoch = 0
+    
+    for (_, _, filenames) in walk("checkpoints"):
+        for filename in filenames:
+            try:
+                epoch = int(re.findall(r'\d+', filename)[0])
+                current_epoch = max(current_epoch, epoch)
+            except:
+                print('invalid file name in checkpoints')
+            
+        break
+
+    return current_epoch + 1
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 32
@@ -9,7 +24,7 @@ IMG_SIZE = 256
 BLANK_SPACE = 12
 L1_LAMBDA = 100
 # set CURRENT_EPOCH to multiple of five for checkpoints and tensorboard
-CURRENT_EPOCH = 0
+CURRENT_EPOCH = get_current_epoch()
 NUM_WORKERS = 8
 
 TRAIN_DIR = "datasets/combined_images/train"
@@ -18,5 +33,6 @@ TEST_DIR = "datasets/combined_images/test"
 EVALUATION_DIR = "datasets/evaluation"
 TENSORBOARD_DIR = "tensorboard"
 
+# set to True if want to load model at checkpoint, CURRENT_EPOCH - 1
 LOAD_MODEL = False
 MODEL_PATH = "checkpoints"
